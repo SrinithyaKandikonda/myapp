bae6ed2b353629f55bf7553c360e38d464ec7ff4
SRINITHYA <SRINITHYA@srinithya1807>
2024-09-24 22:08:50 +0530

    employee added

--- src/com/edubridge/myapp1/model/Employee.java
+++ src/com/edubridge/myapp1/model/Employee.java
@@ -4,7 +4,27 @@ public class Employee {
     private int employee;
     private String employeeName;
     private double employeeSalary;
-
+    
+	public int getEmployee() {
+		return employee;
 	}
+	public void setEmployee(int employee) {
+		this.employee = employee;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+}
+
 
 
